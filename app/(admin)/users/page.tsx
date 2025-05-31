1434f8244b4a742d27f9b42109762946b8ef87c5
MD ALA UDDIN <pial469048@gmail.com>
2025-05-31 18:47:28 +0600

    update: update users page
    - include buttons in the table component for easier data passing

--- app/(admin)/users/page.tsx
+++ app/(admin)/users/page.tsx
@@ -1,7 +1,4 @@
 import DataTable from "@/app/ui/users/data-table";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { LockKeyhole, LockKeyholeOpen, Trash } from "lucide-react";
 import { User, columns } from "@/app/ui/users/columns";
 
 async function getUsers(): Promise<User[]> {
@@ -32,29 +29,7 @@ export default async function Users() {
             <h1 className="mb-10 text-3xl font-bold">User Management</h1>
 
             <div className="w-full overflow-auto">
-                <div className="min-w-max flex flex-col">
-                    {/* Table Heading */}
-                    <div className="w-full mb-2 flex justify-between">
-                        <div className="flex gap-1">
-                            <Button variant="outline" size="sm">
-                                <LockKeyhole /> Block
-                            </Button>
-                            <Button variant="outline" size="sm">
-                                <LockKeyholeOpen />
-                            </Button>
-                            <Button variant="outline" size="sm">
-                                <Trash />
-                            </Button>
-                        </div>
-                        <div className="">
-                            <Input name="filterText" placeholder="Search" />
-                        </div>
-                    </div>
-                    {/* Table */}
-                    <div className="">
-                        <DataTable columns={columns} data={data} />
-                    </div>
-                </div>
+                <DataTable columns={columns} data={data} />
             </div>
         </>
     );
--- app/ui/users/data-table.tsx
+++ app/ui/users/data-table.tsx
@@ -2,9 +2,11 @@
 import { useState } from "react";
 import {
     ColumnDef,
+    ColumnFiltersState,
     SortingState,
     flexRender,
     getCoreRowModel,
+    getFilteredRowModel,
     getSortedRowModel,
     useReactTable,
 } from "@tanstack/react-table";
@@ -17,6 +19,10 @@ import {
     TableRow,
 } from "@/components/ui/table";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { LockKeyhole, LockKeyholeOpen, Trash } from "lucide-react";
+
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
     data: TData[];
@@ -28,6 +34,7 @@ export default function DataTable<TData, TValue>({
 }: DataTableProps<TData, TValue>) {
     const [rowSelection, setRowSelection] = useState({});
     const [sorting, setSorting] = useState<SortingState>([]);
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
     const table = useReactTable({
         data,
@@ -36,60 +43,101 @@ export default function DataTable<TData, TValue>({
         onRowSelectionChange: setRowSelection,
         onSortingChange: setSorting,
         getSortedRowModel: getSortedRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getFilteredRowModel: getFilteredRowModel(),
         state: {
             rowSelection,
-            sorting
-        }
+            sorting,
+            columnFilters,
+        },
     });
 
     return (
-        <div className="border rounded-md">
-            <Table>
-                <TableHeader>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <TableRow key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => (
-                                <TableHead key={header.id}>
-                                    {header.isPlaceholder
-                                        ? null
-                                        : flexRender(
-                                              header.column.columnDef.header,
-                                              header.getContext()
-                                          )}
-                                </TableHead>
+        <div className="min-w-max flex flex-col">
+            {/* Table Heading */}
+            <div className="w-full mb-2 flex justify-between">
+                <div className="flex gap-1">
+                    <Button variant="outline" size="sm">
+                        <LockKeyhole /> Block
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <LockKeyholeOpen />
+                    </Button>
+                    <Button variant="outline" size="sm">
+                        <Trash />
+                    </Button>
+                </div>
+                <div className="">
+                    <Input
+                        value={
+                            (table
+                                .getColumn("email")
+                                ?.getFilterValue() as string) ?? ""
+                        }
+                        onChange={(e) =>
+                            table
+                                .getColumn("email")
+                                ?.setFilterValue(e.target.value)
+                        }
+                        name="filterText"
+                        placeholder="Search"
+                    />
+                </div>
+            </div>
+            {/* Table */}
+            <div className="">
+                <div className="border rounded-md">
+                    <Table>
+                        <TableHeader>
+                            {table.getHeaderGroups().map((headerGroup) => (
+                                <TableRow key={headerGroup.id}>
+                                    {headerGroup.headers.map((header) => (
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(
+                                                      header.column.columnDef
+                                                          .header,
+                                                      header.getContext()
+                                                  )}
+                                        </TableHead>
+                                    ))}
+                                </TableRow>
                             ))}
-                        </TableRow>
-                    ))}
-                </TableHeader>
-                <TableBody>
-                    {table.getRowModel().rows?.length ? (
-                        table.getRowModel().rows.map((row) => (
-                            <TableRow
-                                key={row.id}
-                                data-state={row.getIsSelected() && "selected"}
-                            >
-                                {row.getVisibleCells().map((cell) => (
-                                    <TableCell key={cell.id}>
-                                        {flexRender(
-                                            cell.column.columnDef.cell,
-                                            cell.getContext()
-                                        )}
+                        </TableHeader>
+                        <TableBody>
+                            {table.getRowModel().rows?.length ? (
+                                table.getRowModel().rows.map((row) => (
+                                    <TableRow
+                                        key={row.id}
+                                        data-state={
+                                            row.getIsSelected() && "selected"
+                                        }
+                                    >
+                                        {row.getVisibleCells().map((cell) => (
+                                            <TableCell key={cell.id}>
+                                                {flexRender(
+                                                    cell.column.columnDef.cell,
+                                                    cell.getContext()
+                                                )}
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                ))
+                            ) : (
+                                <TableRow>
+                                    <TableCell
+                                        colSpan={columns.length}
+                                        className="h-24 text-center"
+                                    >
+                                        No Results.
                                     </TableCell>
-                                ))}
-                            </TableRow>
-                        ))
-                    ) : (
-                        <TableRow>
-                            <TableCell
-                                colSpan={columns.length}
-                                className="h-24 text-center"
-                            >
-                                No Results.
-                            </TableCell>
-                        </TableRow>
-                    )}
-                </TableBody>
-            </Table>
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </div>
+            </div>
         </div>
     );
 }
